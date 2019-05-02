bcf5bdd0a22e4b209cd78f800bf803f7d51f9920
Pavel <pavel0408@gmail.com>
2019-05-02 09:45:33 +0300

    поправил конфиг webpack убрал расширения импортируемых файлов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {WelcomeScreen} from "../welcome-screen/welcome-screen.jsx";
+import {WelcomeScreen} from "../welcome-screen/welcome-screen";
 
 export const App = (props) => {
   // eslint-disable-next-line react/prop-types
